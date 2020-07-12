3aa886563ac6947df94d483b4ca0f28ea198069d
John Finlay <finlay_john@hotmail.com>
2020-07-12 08:18:48 -0500

    stringy strings further exploration

--- stringy_strings.rb
+++ stringy_strings.rb
@@ -1,7 +1,15 @@
-def stringy(num)
-  result = ''
-  (1..num).each { |number| result << (number % 2).to_s }
-  result
+def stringy(num, start = 1)
+  result = []
+  num.times do |index|
+    if start == 0
+      number = index.even? ? 0 : 1
+    else
+      number = index.even? ? 1 : 0
+    end
+    result << number
+  end
+      
+  result.join
 end
 
 
@@ -9,3 +17,8 @@ puts stringy(6) == '101010'
 puts stringy(9) == '101010101'
 puts stringy(4) == '1010'
 puts stringy(7) == '1010101'
+puts
+puts stringy(6, 0) == '010101'
+puts stringy(9, 0) == '010101010'
+puts stringy(4, 0) == '0101'
+puts stringy(7, 0) == '0101010'
