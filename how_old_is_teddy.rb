47e03982634ec678160974684aa2381d9819325d
John Finlay <finlay_john@hotmail.com>
2020-07-17 14:05:26 -0500

    how old is teddy exercise

---
+++ how_old_is_teddy.rb
@@ -0,0 +1,19 @@
+=begin
+    1. Understand the problem
+        Build a program that randomly generates and prints Teddy's age. To get
+        the age, you should generate a random number between 20 and 200.
+
+        Example Output
+
+        Teddy is 69 years old!
+
+    2. Set up your test cases
+
+    3. Describe your inputs, outputs, and data structures
+
+    4. Describe your algorithm
+
+    5. Begin coding
+=end
+
+puts "Teddy is #{rand(20..200)} years old!"
