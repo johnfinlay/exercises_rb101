3811fc4138ccee4b7ef48a702b4ff9b7acb8166f
John Finlay <finlay_john@hotmail.com>
2020-08-01 16:20:00 -0500

    palindromic strings

---
+++ easy3/palindromic_strings.rb
@@ -0,0 +1,8 @@
+def palindrome?(str)
+  str == str.reverse
+end
+
+p palindrome?('madam') == true
+p palindrome?('Madam') == false
+p palindrome?("madam i'm adam") == false
+p palindrome?('356653') == true
