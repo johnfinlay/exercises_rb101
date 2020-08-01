f24804912c7dfd6c271d6d99d5c16cb92662e83b
John Finlay <finlay_john@hotmail.com>
2020-08-01 14:59:44 -0500

    sum or product exercise

---
+++ sum_or_product.rb
@@ -0,0 +1,15 @@
+
+puts "Please enter an integer greater than 0:"
+num = gets.chomp.to_i
+
+puts "Enter 's' to compute the sum, 'p' to compute the product:"
+calc = gets.chomp
+
+result = 1
+if calc.downcase == 's'
+  (2..num).each { |int| result += int }
+  puts "The sum of the integers between 1 and #{num} is #{result}."
+elsif calc.downcase == 'p'
+  (2..num).each { |int| result *= int }
+  puts "The product of the integers between 1 and #{num} is #{result}."
+end
