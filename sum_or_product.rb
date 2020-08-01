dfb0ae3b0479117c224389718866cdd799c1229b
John Finlay <finlay_john@hotmail.com>
2020-08-01 15:07:15 -0500

    sum or product refactor

--- sum_or_product.rb
+++ sum_or_product.rb
@@ -5,13 +5,10 @@ num = gets.chomp.to_i
 puts "Enter 's' to compute the sum, 'p' to compute the product:"
 calc = gets.chomp
 
-result = 1
 if calc.downcase == 's'
-  (2..num).each { |int| result += int }
-  puts "The sum of the integers between 1 and #{num} is #{result}."
+  puts "The sum of the integers between 1 and #{num} is #{(1..num).reduce(:+)}."
 elsif calc.downcase == 'p'
-  (2..num).each { |int| result *= int }
-  puts "The product of the integers between 1 and #{num} is #{result}."
+  puts "The product of the integers between 1 and #{num} is #{(1..num).reduce(:*)}."
 else
   puts "Invalid operation."
 end
