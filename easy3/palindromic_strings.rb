d9a3556b6eb50190ae76c12bf0d521be7be3de49
John Finlay <finlay_john@hotmail.com>
2020-08-01 16:30:47 -0500

    palindromic strings part 2

--- easy3/palindromic_strings.rb
+++ easy3/palindromic_strings.rb
@@ -2,7 +2,19 @@ def palindrome?(str)
   str == str.reverse
 end
 
-p palindrome?('madam') == true
-p palindrome?('Madam') == false
-p palindrome?("madam i'm adam") == false
-p palindrome?('356653') == true
+def real_palindrome?(str)
+  str = str.downcase.delete('^a-z0-9')
+  palindrome?(str)
+end
+
+# p palindrome?('madam') == true
+# p palindrome?('Madam') == false
+# p palindrome?("madam i'm adam") == false
+# p palindrome?('356653') == true
+
+p real_palindrome?('madam') == true
+p real_palindrome?('Madam') == true           # (case does not matter)
+p real_palindrome?("Madam, I'm Adam") == true # (only alphanumerics matter)
+p real_palindrome?('356653') == true
+p real_palindrome?('356a653') == true
+p real_palindrome?('123ab321') == false
