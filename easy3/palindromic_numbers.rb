c3f8922b70305af641ca0a4dbaf4acff38c50c29
John Finlay <finlay_john@hotmail.com>
2020-08-04 06:37:08 -0500

    palindromic numbers

---
+++ easy3/palindromic_numbers.rb
@@ -0,0 +1,11 @@
+require_relative 'palindromic_strings'
+
+def palindromic_number?(num)
+  # num.to_s == num.to_s.reverse
+  palindrome?(num.to_s)
+end
+
+p palindromic_number?(34543) == true
+p palindromic_number?(123210) == false
+p palindromic_number?(22) == true
+p palindromic_number?(5) == true
--- easy3/palindromic_strings.rb
+++ easy3/palindromic_strings.rb
@@ -12,9 +12,9 @@ end
 # p palindrome?("madam i'm adam") == false
 # p palindrome?('356653') == true
 
-p real_palindrome?('madam') == true
-p real_palindrome?('Madam') == true           # (case does not matter)
-p real_palindrome?("Madam, I'm Adam") == true # (only alphanumerics matter)
-p real_palindrome?('356653') == true
-p real_palindrome?('356a653') == true
-p real_palindrome?('123ab321') == false
+# p real_palindrome?('madam') == true
+# p real_palindrome?('Madam') == true           # (case does not matter)
+# p real_palindrome?("Madam, I'm Adam") == true # (only alphanumerics matter)
+# p real_palindrome?('356653') == true
+# p real_palindrome?('356a653') == true
+# p real_palindrome?('123ab321') == false
