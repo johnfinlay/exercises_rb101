f2d775796c63fc6ad71e7ab389af683a811f61fa
John Finlay <finlay_john@hotmail.com>
2020-08-05 06:14:41 -0500

    leap years part 2

--- easy4/leap_years.rb
+++ easy4/leap_years.rb
@@ -1,4 +1,6 @@
 def leap_year?(year)
+  return (year % 4).zero? if year < 1752
+
   (year % 100).zero? ? (year % 400).zero? : (year % 4).zero?
 end
 
@@ -11,7 +13,7 @@ p leap_year?(240001) == false
 p leap_year?(2000) == true
 p leap_year?(1900) == false
 p leap_year?(1752) == true
-p leap_year?(1700) == false
+p leap_year?(1700) == true
 p leap_year?(1) == false
-p leap_year?(100) == false
+p leap_year?(100) == true
 p leap_year?(400) == true
