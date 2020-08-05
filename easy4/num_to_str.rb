f73729bf0d0b7a3aaaf45b9f131584c97dbe0887
John Finlay <finlay_john@hotmail.com>
2020-08-05 07:24:14 -0500

    begin num to str

---
+++ easy4/num_to_str.rb
@@ -0,0 +1,13 @@
+DIGITS = {
+  0 => '0', 1 => '1', 2 => '2', 3 => '3', 4 => '4',
+  5 => '5', 6 => '6', 7 => '7', 8 => '8', 9 => '9'
+}
+
+def integer_to_string(num)
+  arr = []
+  
+end
+
+integer_to_string(4321) == '4321'
+integer_to_string(0) == '0'
+integer_to_string(5000) == '5000'
