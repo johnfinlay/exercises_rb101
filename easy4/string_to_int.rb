468a9722a44df523f6a0af1d5cdb2df306197c10
John Finlay <finlay_john@hotmail.com>
2020-08-05 06:57:20 -0500

    refactor string to integer

--- easy4/string_to_int.rb
+++ easy4/string_to_int.rb
@@ -1,18 +1,12 @@
+DIGITS = {
+  '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4,
+  '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9
+}
+
 def string_to_integer(str)
   result = 0
   str.chars.each do |digit|
-    result *= 10
-    case digit
-    when '1' then result += 1
-    when '2' then result += 2
-    when '3' then result += 3
-    when '4' then result += 4
-    when '5' then result += 5
-    when '6' then result += 6
-    when '7' then result += 7
-    when '8' then result += 8
-    when '9' then result += 9
-    end
+    result = 10 * result + DIGITS[digit]
   end
   result
 end
