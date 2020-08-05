77135e6e178ad167b8901401e8bcfe1d47affcf5
John Finlay <finlay_john@hotmail.com>
2020-08-05 09:46:09 -0500

    signed num to str

--- easy4/num_to_str.rb
+++ easy4/num_to_str.rb
@@ -4,9 +4,23 @@ DIGITS = {
 }
 
 def integer_to_string(num)
-  num.digits.reverse.map { |n| DIGITS[n] }.join
+  if num >= 2
+    num.digits.reverse.map { |n| DIGITS[n] }.join
+  else
+    DIGITS[num]
+  end
+end
+
+def signed_integer_to_string(num)
+  str = num > 0 ? integer_to_string(num) : integer_to_string(num * -1)
+  str.prepend('-') if num < 0
+  str.prepend('+') if num > 0
+  str
 end
 
 p integer_to_string(4321) == '4321'
 p integer_to_string(0) == '0'
 p integer_to_string(5000) == '5000'
+p signed_integer_to_string(4321) == '+4321'
+p signed_integer_to_string(-123) == '-123'
+p signed_integer_to_string(0) == '0'
