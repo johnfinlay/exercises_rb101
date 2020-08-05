aae80fcdde6b2dad5f4c8a4ac216789e8194bb1e
John Finlay <finlay_john@hotmail.com>
2020-08-05 09:24:04 -0500

    num to str

--- easy4/num_to_str.rb
+++ easy4/num_to_str.rb
@@ -4,10 +4,9 @@ DIGITS = {
 }
 
 def integer_to_string(num)
-  arr = []
-  
+  num.digits.reverse.map { |n| DIGITS[n] }.join
 end
 
-integer_to_string(4321) == '4321'
-integer_to_string(0) == '0'
-integer_to_string(5000) == '5000'
+p integer_to_string(4321) == '4321'
+p integer_to_string(0) == '0'
+p integer_to_string(5000) == '5000'
