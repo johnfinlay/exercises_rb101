518caab41babb4902511e3772b9c6a0cb09b95b2
John Finlay <finlay_john@hotmail.com>
2020-08-07 06:49:09 -0500

    letter counter part 2

--- easy5/letter_counter.rb
+++ easy5/letter_counter.rb
@@ -2,11 +2,11 @@ require 'pry'
 
 def word_sizes(words)
   result = Hash.new(0)
-  words.split.each { |word| result[word.length] += 1 }
+  words.split.each { |word| result[word.delete('^a-zA-Z').length] += 1 }
   result
 end
 
-p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
-p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
-p word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
+p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
+p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
+p word_sizes("What's up doc?") == { 5 => 1, 2 => 1, 3 => 1 }
 p word_sizes('') == {}
