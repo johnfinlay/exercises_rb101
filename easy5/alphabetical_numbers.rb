5ff98598001d99202adac616ce8f28428b7f5697
John Finlay <finlay_john@hotmail.com>
2020-08-07 07:05:52 -0500

    aplhabetical numbers refactor

--- easy5/alphabetical_numbers.rb
+++ easy5/alphabetical_numbers.rb
@@ -1,14 +1,10 @@
-NUMBER_WORDS = {
-  0 => 'zero', 1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five',
-  6 => 'six', 7 => 'seven', 8 => 'eight', 9 => 'nine', 10 => 'ten',
-  11 => 'eleven', 12 => 'twelve', 13 => 'thirteen', 14 => 'fourteen',
-  15 => 'fifteen', 16 => 'sixteen', 17 => 'seventeen', 18 => 'eighteen',
-  19 => 'nineteen'
-}
+NUMBER_WORDS = %w(zero one two three four five
+                  six seven eight nine ten
+                  eleven twelve thirteen fourteen
+                  fifteen sixteen seventeen eighteen nineteen)
 
 def alphabetic_number_sort(arr)
-  alpha = arr.map { |num| NUMBER_WORDS[num] }.sort
-  alpha.map { |text| NUMBER_WORDS.key(text) }
+  arr.sort_by { |num| NUMBER_WORDS[num] }
 end
 
 p alphabetic_number_sort((0..19).to_a) == [
