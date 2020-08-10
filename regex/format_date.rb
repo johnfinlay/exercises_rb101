5fcfd0f93a59a38b1541262fdfd841385a14be4c
John Finlay <finlay_john@hotmail.com>
2020-08-10 07:17:15 -0500

    regex format date part 2

--- regex/format_date.rb
+++ regex/format_date.rb
@@ -1,6 +1,7 @@
 def format_date(my_date)
-  my_date.gsub(/\A(\d{4})-(\d{2})-(\d{2})\z/, '\3.\2.\1')
+  my_date.gsub(/\A(\d{4})([-\/])(\d{2})\2(\d{2})\z/, '\4.\3.\1')
 end
 
 p format_date('2016-06-17') == '17.06.2016'
-p format_date('2016/06/17') == '2016/06/17' # (no change)
+p format_date('2017/05/03') == '03.05.2017'
+p format_date('2015/01-31') == '2015/01-31' # (no change)
