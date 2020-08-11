9e2bf504fe79b7e865faa1ce2173b5c05069f419
John Finlay <finlay_john@hotmail.com>
2020-08-11 07:23:52 -0500

    refactor cute angles further exploration

--- easy6/cute_angles.rb
+++ easy6/cute_angles.rb
@@ -5,13 +5,8 @@ MINUTES_PER_DEGREE = 60
 SECONDS_PER_MINUTE = 60
 SECONDS_PER_DEGREE = MINUTES_PER_DEGREE * SECONDS_PER_MINUTE
 
-def fix_degrees(flt)
-  flt = flt % 360 until flt < 360
-  flt
-end
-
 def dms(flt)
-  degrees_float = flt.negative? ? (360 - fix_degrees(flt * -1)) : fix_degrees(flt)
+  degrees_float = flt % 360
   total_seconds = (degrees_float * SECONDS_PER_DEGREE).round
   degrees, remaining_seconds = total_seconds.divmod(SECONDS_PER_DEGREE)
   minutes, seconds = remaining_seconds.divmod(SECONDS_PER_MINUTE)
