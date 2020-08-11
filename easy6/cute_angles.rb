ddf800eb0388a558f220fc5986033f1fccc7de40
John Finlay <finlay_john@hotmail.com>
2020-08-11 07:07:13 -0500

    cute angles refactor

--- easy6/cute_angles.rb
+++ easy6/cute_angles.rb
@@ -1,14 +1,15 @@
 require 'pry'
 
 DEGREE = "\xC2\xB0"
+MINUTES_PER_DEGREE = 60
+SECONDS_PER_MINUTE = 60
+SECONDS_PER_DEGREE = MINUTES_PER_DEGREE * SECONDS_PER_MINUTE
 
 def dms(flt)
-  d = flt.to_i
-  m = flt.zero? ? 0 : (flt % d) * 60
-  s = m.zero? ? 0 : (m % m.to_i) * 60
-  result = "#{flt.to_i}#{DEGREE}#{format("%02d", m)}'#{format("%02d", s)}\""
-  puts result
-  result
+  total_seconds = (flt * SECONDS_PER_DEGREE).round
+  degrees, remaining_seconds = total_seconds.divmod(SECONDS_PER_DEGREE)
+  minutes, seconds = remaining_seconds.divmod(SECONDS_PER_MINUTE)
+  format(%(#{degrees}#{DEGREE}%02d'%02d"), minutes, seconds)
 end
 
 p dms(30) == %(30°00'00")
