5dc05f80959eb292e003ad7d91db1f70e3217b69
John Finlay <finlay_john@hotmail.com>
2020-08-11 07:18:56 -0500

    cute angles further exploration

--- easy6/cute_angles.rb
+++ easy6/cute_angles.rb
@@ -5,8 +5,14 @@ MINUTES_PER_DEGREE = 60
 SECONDS_PER_MINUTE = 60
 SECONDS_PER_DEGREE = MINUTES_PER_DEGREE * SECONDS_PER_MINUTE
 
+def fix_degrees(flt)
+  flt = flt % 360 until flt < 360
+  flt
+end
+
 def dms(flt)
-  total_seconds = (flt * SECONDS_PER_DEGREE).round
+  degrees_float = flt.negative? ? (360 - fix_degrees(flt * -1)) : fix_degrees(flt)
+  total_seconds = (degrees_float * SECONDS_PER_DEGREE).round
   degrees, remaining_seconds = total_seconds.divmod(SECONDS_PER_DEGREE)
   minutes, seconds = remaining_seconds.divmod(SECONDS_PER_MINUTE)
   format(%(#{degrees}#{DEGREE}%02d'%02d"), minutes, seconds)
@@ -18,3 +24,6 @@ p dms(254.6) == %(254°36'00")
 p dms(93.034773) == %(93°02'05")
 p dms(0) == %(0°00'00")
 p dms(360) == %(360°00'00") || dms(360) == %(0°00'00")
+p dms(400) == %(40°00'00")
+p dms(-40) == %(320°00'00")
+p dms(-420) == %(300°00'00")
