2489e53ab075f1c48352921ed7f527b78bd1f265
John Finlay <finlay_john@hotmail.com>
2020-08-19 06:53:34 -0500

    interleave further exploration

--- easy7/interleave.rb
+++ easy7/interleave.rb
@@ -1,10 +1,13 @@
 def interleave(arr1, arr2)
   result = []
-  arr1.each_index do |i|
-    result << arr1[i]
-    result << arr2[i]
-  end
+  arr1.each_index { |i| result << arr1[i] << arr2[i] }
   result
 end
 
+def interleave_zip(arr1, arr2)
+  arr1.zip(arr2).flatten
+end
+
 p interleave([1, 2, 3], ['a', 'b', 'c']) == [1, 'a', 2, 'b', 3, 'c']
+
+p interleave_zip([1, 2, 3], ['a', 'b', 'c']) == [1, 'a', 2, 'b', 3, 'c']
