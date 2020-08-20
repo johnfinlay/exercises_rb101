8caaf0574d934903fe68d262f69c1196f1260f19
John Finlay <finlay_john@hotmail.com>
2020-08-20 06:34:27 -0500

    capitalize words further exploration

--- easy7/capitalize_words.rb
+++ easy7/capitalize_words.rb
@@ -1,5 +1,11 @@
 def word_cap(str)
-  str.split.map { |word| word.capitalize }.join(' ')
+  str.split.map do |word|
+    new_word = ''
+    word.chars.each_with_index do |char, index|
+      index.zero? ? (new_word << char.upcase) : (new_word << char.downcase)
+    end
+    new_word
+  end.join(' ')
 end
 
 p word_cap('four score and seven') == 'Four Score And Seven'
