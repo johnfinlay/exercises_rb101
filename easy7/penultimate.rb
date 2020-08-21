6f10653025a7385915a6fe4e6fd9e481efc270aa
John Finlay <finlay_john@hotmail.com>
2020-08-21 07:59:17 -0500

    the end is near but not here further exploration

--- easy7/penultimate.rb
+++ easy7/penultimate.rb
@@ -2,5 +2,17 @@ def penultimate(str)
   str.split[-2]
 end
 
-p penultimate('last word') == 'last'
-p penultimate('Launch School is great!') == 'is'
+def middle_word(str)
+  words = str.split
+  return '' if words.empty? || words.count.even?
+
+  words[(words.count / 2)]
+end
+
+# p penultimate('last word') == 'last'
+# p penultimate('Launch School is great!') == 'is'
+
+p middle_word('I am middle') == 'am'
+p middle_word('') == ''
+p middle_word('No middle') == ''
+p middle_word('Uno') == 'Uno'
