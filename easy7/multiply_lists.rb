6d58958482417760f412992adc791afbc2073a57
John Finlay <finlay_john@hotmail.com>
2020-08-21 07:41:07 -0500

    multiply lists further exploration

--- easy7/multiply_lists.rb
+++ easy7/multiply_lists.rb
@@ -1,5 +1,5 @@
 def multiply_list(arr1, arr2)
-  arr1.map.with_index { |num, index| num * arr2[index] }
+  arr1.zip(arr2).map { |arr| arr.reduce(:*) }
 end
 
 p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
