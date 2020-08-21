8c050b4063c3dab1cf349e10ec58628b20b6d9c7
John Finlay <finlay_john@hotmail.com>
2020-08-21 06:57:33 -0500

    staggered caps part 2

--- easy7/staggered_caps.rb
+++ easy7/staggered_caps.rb
@@ -1,15 +1,20 @@
-def staggered_case(str, up_down)
+require 'pry'
+require 'pry-byebug'
+
+def staggered_case(str)
   new_word = ''
-  str.chars.each_with_index do |char, index|
-    if up_down == 'up'
-      new_word << (index.even? ? char.upcase : char.downcase)
-    else
-      new_word << (index.even? ? char.downcase : char.upcase)
-    end
+  upper = true
+  str.chars.each do |char|
+    new_word << if char.match?(/[a-z]|[A-Z]/)
+                  upper ? char.upcase : char.downcase
+                else
+                  char
+                end
+    upper = !upper if char.match?(/[a-z]|[A-Z]/)
   end
   new_word
 end
 
-p staggered_case('I Love Launch School!', 'down') == 'i lOvE LaUnCh sChOoL!'
-p staggered_case('ALL_CAPS', 'down') == 'AlL_CaPs'
-p staggered_case('ignore 77 the 444 numbers', 'down') == 'IgNoRe 77 ThE 444 NuMbErS'
+p staggered_case('I Love Launch School!') == 'I lOvE lAuNcH sChOoL!'
+p staggered_case('ALL CAPS') == 'AlL cApS'
+p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 nUmBeRs'
