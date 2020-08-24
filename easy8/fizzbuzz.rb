5d84f3d5c1b5743a30f6211a35ba642a2075e9a5
John Finlay <finlay_john@hotmail.com>
2020-08-24 07:06:32 -0500

    refactor fizzbuzz

--- easy8/fizzbuzz.rb
+++ easy8/fizzbuzz.rb
@@ -1,17 +1,18 @@
 def fizzbuzz(start_num, end_num)
-  results = []
+  result = []
   start_num.upto(end_num) do |num|
-    if (num % 3).zero? && (num % 5).zero?
-      results << 'FizzBuzz'
-    elsif (num % 3).zero?
-      results << 'Fizz'
-    elsif (num % 5).zero?
-      results << 'Buzz'
-    else
-      results << num
-    end
+    result << fizzbuzz_value(num)
   end
-  puts results.join(', ')
+  puts result.join(', ')
+end
+
+def fizzbuzz_value(number)
+  case 
+  when (number % 3).zero? && (number % 5).zero? then 'FizzBuzz'
+  when (number % 3).zero? then 'Fizz'
+  when (number % 5).zero? then 'Buzz'
+  else number
+  end
 end
 
 fizzbuzz(1, 15) # -> 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz
