2d7f53526551364fff9909864dabbe0baaedca04
John Finlay <finlay_john@hotmail.com>
2020-08-24 07:29:35 -0500

    double char part 2

--- easy8/double_char.rb
+++ easy8/double_char.rb
@@ -4,6 +4,19 @@ def repeater(str)
   result
 end
 
-p repeater('Hello') == "HHeelllloo"
-p repeater("Good job!") == "GGoooodd  jjoobb!!"
-p repeater('') == ''
+def double_consonants(str)
+  result = ''
+  str.each_char do |char|
+    result << char
+    result << char unless char.match?(/([aeiou0-9]|[\W])/i) 
+  end
+  result
+end
+
+# p repeater('Hello') == "HHeelllloo"
+# p repeater("Good job!") == "GGoooodd  jjoobb!!"
+# p repeater('') == ''
+p double_consonants('String') == "SSttrrinngg"
+p double_consonants("Hello-World!") == "HHellllo-WWorrlldd!"
+p double_consonants("July 4th") == "JJullyy 4tthh"
+p double_consonants('') == ""
