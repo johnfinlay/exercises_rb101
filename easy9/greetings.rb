3fec05f2e933104446772cde59334ea7c8e42a9a
John Finlay <finlay_john@hotmail.com>
2020-08-25 06:40:49 -0500

    greetings corrected return value.

--- easy9/greetings.rb
+++ easy9/greetings.rb
@@ -1,6 +1,6 @@
 def greetings(person, career)
-  puts "Hello, #{person.join(' ')}! Nice to have a #{career[:title]} #{career[:occupation]} around."
+  "Hello, #{person.join(' ')}! Nice to have a #{career[:title]} #{career[:occupation]} around."
 end
 
-greetings(['John', 'Q', 'Doe'], { title: 'Master', occupation: 'Plumber' })
+puts greetings(['John', 'Q', 'Doe'], { title: 'Master', occupation: 'Plumber' })
 # => Hello, John Q Doe! Nice to have a Master Plumber around.
