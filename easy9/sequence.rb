43b0148fa0d115e6c31bcb8d9d7507825a1ecb55
John Finlay <finlay_john@hotmail.com>
2020-08-25 07:25:51 -0500

    sequence further exploration

--- easy9/sequence.rb
+++ easy9/sequence.rb
@@ -1,9 +1,9 @@
 def sequence(num)
-  result = []
-  (1..num).each { |count| result << count }
-  result
+  result = (1..num.abs).to_a
+  num.negative? ? result.map!(&:-@) : result
 end
 
 p sequence(5) == [1, 2, 3, 4, 5]
 p sequence(3) == [1, 2, 3]
 p sequence(1) == [1]
+p sequence(-4) == [-1, -2, -3, -4]
