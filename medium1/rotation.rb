768ff1c597e67431cfa9fb33945c4864ea8871f0
John Finlay <finlay_john@hotmail.com>
2020-08-26 06:11:17 -0500

    rotation part 1 further exploration

--- medium1/rotation.rb
+++ medium1/rotation.rb
@@ -4,6 +4,14 @@ def rotate_array(arr)
   result
 end
 
+def rotate_string(str)
+  rotate_array(str.chars).join
+end
+
+def rotate_integer(int)
+  rotate_array(int.to_s.chars).join.to_i
+end
+
 p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
 p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
 p rotate_array(['a']) == ['a']
@@ -11,3 +19,7 @@ p rotate_array(['a']) == ['a']
 x = [1, 2, 3, 4]
 p rotate_array(x) == [2, 3, 4, 1]   # => true
 p x == [1, 2, 3, 4]                 # => true
+
+p rotate_string('abcd') == 'bcda'
+
+p rotate_integer(1234) == 2341
