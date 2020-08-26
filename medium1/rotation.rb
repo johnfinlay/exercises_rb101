1b3b9a3b5c97d1a4a457fbd73e63d9e9d816ffc4
John Finlay <finlay_john@hotmail.com>
2020-08-26 06:29:47 -0500

    rotation part 2

--- medium1/rotation.rb
+++ medium1/rotation.rb
@@ -12,14 +12,28 @@ def rotate_integer(int)
   rotate_array(int.to_s.chars).join.to_i
 end
 
-p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
-p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
-p rotate_array(['a']) == ['a']
+def rotate_rightmost_digits(int, digits)
+  int_arr = int.to_s.chars
+  result = int_arr.slice(0, (int_arr.count - digits))
+  result << rotate_array(int_arr.slice(-digits, digits))
+  result.flatten.join.to_i
+end
 
-x = [1, 2, 3, 4]
-p rotate_array(x) == [2, 3, 4, 1]   # => true
-p x == [1, 2, 3, 4]                 # => true
+p rotate_rightmost_digits(735291, 1) == 735291
+p rotate_rightmost_digits(735291, 2) == 735219
+p rotate_rightmost_digits(735291, 3) == 735912
+p rotate_rightmost_digits(735291, 4) == 732915
+p rotate_rightmost_digits(735291, 5) == 752913
+p rotate_rightmost_digits(735291, 6) == 352917
 
-p rotate_string('abcd') == 'bcda'
+# p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+# p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+# p rotate_array(['a']) == ['a']
 
-p rotate_integer(1234) == 2341
+# x = [1, 2, 3, 4]
+# p rotate_array(x) == [2, 3, 4, 1]   # => true
+# p x == [1, 2, 3, 4]                 # => true
+
+# p rotate_string('abcd') == 'bcda'
+
+# p rotate_integer(1234) == 2341
