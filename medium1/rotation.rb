29a5b65372329ead33f57c818ebddc25ae31506e
John Finlay <finlay_john@hotmail.com>
2020-08-26 06:05:00 -0500

    rotation

---
+++ medium1/rotation.rb
@@ -0,0 +1,13 @@
+def rotate_array(arr)
+  result = arr.map.to_a
+  result << result.shift
+  result
+end
+
+p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+p rotate_array(['a']) == ['a']
+
+x = [1, 2, 3, 4]
+p rotate_array(x) == [2, 3, 4, 1]   # => true
+p x == [1, 2, 3, 4]                 # => true
