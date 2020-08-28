6786c3700836990a931c8770ef6b7c5f34cbd72a
John Finlay <finlay_john@hotmail.com>
2020-08-28 07:30:51 -0500

    fibonacci last digit

--- medium1/fibonacci_recursion.rb
+++ medium1/fibonacci_recursion.rb
@@ -4,10 +4,22 @@ def fibonacci(nth)
   last
 end
 
-p fibonacci(1) == 1
-p fibonacci(2) == 1
-p fibonacci(3) == 2
-p fibonacci(4) == 3
-p fibonacci(5) == 5
-p fibonacci(12) == 144
-p fibonacci(20) == 6765
+def fibonacci_last(nth)
+  last_2 = [1, 1]
+  3.upto(nth) { last_2 = [last_2.last, (last_2.first + last_2.last) % 10] }
+  last_2.last
+end
+
+# p fibonacci(1) == 1
+# p fibonacci(2) == 1
+# p fibonacci(3) == 2
+# p fibonacci(4) == 3
+# p fibonacci(5) == 5
+# p fibonacci(12) == 144
+# p fibonacci(20) == 6765
+p fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
+p fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
+p fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
+p fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
+p fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
+p fibonacci_last(123456789) # -> 4
