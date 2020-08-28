12203c94e994bbc88b17a5f5a1ec3ee5a46a5825
John Finlay <finlay_john@hotmail.com>
2020-08-28 06:23:20 -0500

    word to digit further exploration

--- medium1/word_to_digit.rb
+++ medium1/word_to_digit.rb
@@ -1,3 +1,6 @@
+require 'pry'
+require 'pry-byebug'
+
 DIGITS = {
   'one' => '1',
   'two' => '2',
@@ -14,4 +17,29 @@ def word_to_digit(sentence)
   sentence
 end
 
-p word_to_digit('Please call me at five five five one two three four. Thanks.') == 'Please call me at 5 5 5 1 2 3 4. Thanks.'
+# Further Exploration
+
+def word_to_digit_with_squeeze(sentence)
+  words = sentence.split
+  prev_word = ''
+  result = []
+  words.each do |word|
+    punctuation = ''
+    word, punctuation = word[0..-2], word[-1] if word.end_with?('.', ',', '!', '?')
+    if DIGITS.keys.include?(word)
+      #binding.pry
+      prev_word << DIGITS[word]
+      if punctuation.length > 0
+        result << (prev_word + punctuation)
+        prev_word = ''
+      end
+    else
+      result << prev_word if prev_word.length > 0
+      result << word
+      prev_word = ''
+    end
+  end
+  result.join(' ')
+end
+
+p word_to_digit_with_squeeze('Please 3 4 call me at five five five one two three four. Thanks.') #== 'Please 3 4 call me at 5551234. Thanks.'
