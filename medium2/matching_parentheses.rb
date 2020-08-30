063fb4651a0491562411a1e70db0e350010a3d09
John Finlay <finlay_john@hotmail.com>
2020-08-30 09:19:49 -0500

    matching parentheses further exploration

--- medium2/matching_parentheses.rb
+++ medium2/matching_parentheses.rb
@@ -1,21 +1,21 @@
-def balanced?(str)
-  open_paren_count = 0
+def balanced?(str, delimiters = ['(', ')'])
+  count = 0
   str.chars.each do |char|
-    if char == '('
-      open_paren_count += 1
-    elsif char == ')'
-      return false if open_paren_count.zero?
+    if char == delimiters[0]
+      count += 1
+    elsif char == delimiters[1]
+      return false if count.zero?
 
-      open_paren_count -= 1
+      count -= 1
     end
   end
-  open_paren_count.zero?
+  count.zero?
 end
 
 p balanced?('What (is) this?') == true
 p balanced?('What is) this?') == false
 p balanced?('What (is this?') == false
-p balanced?('((What) (is this))?') == true
+p balanced?('{{What} {is this}}?', ['{', '}']) == true
 p balanced?('((What)) (is this))?') == false
 p balanced?('Hey!') == true
 p balanced?(')Hey!(') == false
