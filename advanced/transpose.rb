4fe5a99fb2c0a65278cae0477992205f22c14598
John Finlay <finlay_john@hotmail.com>
2020-09-09 07:13:30 -0500

    transpose MxN

--- advanced/transpose.rb
+++ advanced/transpose.rb
@@ -1,43 +1,17 @@
-=begin
-    1. Understand the problem
-        Write a method that takes a 3 x 3 matrix in Array of Arrays format and 
-        returns the transpose of the original matrix. Note that there is a 
-        Array#transpose method that does this -- you may not use it for this 
-        exercise. You also are not allowed to use the Matrix class from the 
-        standard library. Your task is to do this yourself.
-
-        Take care not to modify the original matrix: you must produce a new 
-        matrix and leave the original matrix unchanged.
-
-    2. Set up your test cases
-
-    3. Describe your inputs, outputs, and data structures
-          Input is a nested array
-          Output is a nested array
-    4. Describe your algorithm
-        - Initiate a new array
-        - Loop the outer array with index
-          - Loop the inner array with index
-          - Assign the element[outer_index][inner_index] to the new_array[inner_index][outer_index]
-    5. Begin coding
-=end
+require 'pry'
+require 'pry-byebug'
 
 def transpose(matrix)
   result = []
-  (0..2).each do |column_index|
-    new_row = (0..2).map { |row_index| matrix[row_index][column_index] }
+  (0..(matrix[0].count - 1)).each do |column_index|
+    new_row = (0..(matrix.count - 1)).map { |row_index| matrix[row_index][column_index] }
     result << new_row
   end
   result
 end
 
-matrix = [
-  [1, 5, 8],
-  [4, 7, 2],
-  [3, 9, 6]
-]
-
-new_matrix = transpose(matrix)
-
-p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
-p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
+p transpose([[1, 2, 3, 4]]) == [[1], [2], [3], [4]]
+p transpose([[1], [2], [3], [4]]) == [[1, 2, 3, 4]]
+p transpose([[1, 2, 3, 4, 5], [4, 3, 2, 1, 0], [3, 7, 8, 6, 2]]) ==
+  [[1, 4, 3], [2, 3, 7], [3, 2, 8], [4, 1, 6], [5, 0, 2]]
+p transpose([[1]]) == [[1]]
