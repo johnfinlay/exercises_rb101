586f7f5ddf35242e99160ba7191dec42f2370c00
John Finlay <finlay_john@hotmail.com>
2020-09-11 13:52:30 -0500

    getting things done further exploration

--- debugging/getting_things_done.rb
+++ debugging/getting_things_done.rb
@@ -1,5 +1,5 @@
 def move(n, from_array, to_array)
-  return nil if n.zero?
+  return nil if n.zero? || from_array.empty?
   to_array << from_array.shift
   move(n - 1, from_array, to_array)
 end
@@ -9,7 +9,7 @@ end
 todo = ['study', 'walk the dog', 'coffee with Tom']
 done = ['apply sunscreen', 'go to the beach']
 
-move(2, todo, done)
+move(8, todo, done)
 
 p todo # should be: ['coffee with Tom']
 p done # should be: ['apply sunscreen', 'go to the beach', 'study', 'walk the dog']
